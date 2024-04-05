fee1f79d50e909370c2d24c15d641e39b91ebe1b
Utku İpek <utkuipek@Utkus-MacBook-Air.local>
2024-04-06 02:49:46 +0300

    main.go updated

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gofiber/fiber/v2"
+	config "github.com/ipekutku/playlist-generator"
+	auth "github.com/ipekutku/playlist-generator/pkg/auth"
+	log "github.com/ipekutku/playlist-generator/pkg/log"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Initialize logger
+	logger := log.NewLogger()
+
+	// Defer Sync to ensure all logs are flushed before the program exits
+	defer logger.Sync()
+
+	// Load configuration
+	appConfig := config.SpotifyConfig()
+
+	// Initialize Spotify service and AuthHandler
+	spotify := auth.NewSpotifyService(&appConfig.SpotifyLoginConfig)
+	auth := auth.NewAuthHandler(spotify, logger, &appConfig.SpotifyLoginConfig)
+
+	// Initialize Fiber app
+	app := fiber.New()
+
+	// Define routes
+	app.Get("/login", auth.SpotifyLogin)
+	app.Post("/callback", auth.SpotifyCallback)
+
+	// Start server
+	if err := app.Listen(":8080"); err != nil {
+		logger.Error("Failed to start server", "error", err)
+	}
 }
